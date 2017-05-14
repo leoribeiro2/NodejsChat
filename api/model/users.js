694e8f687363ea62cd9df0217cff534dfbc9c5d6
Leonardo Ribeiro <contato@leoribeiroweb.com.br>
2017-05-14 02:14:47 -0300

    web app, socket io and more

---
+++ api/model/users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+// const passportLocalMongoose = require('passport-local-mongoose');
+
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  slug: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+// User.plugin(passportLocalMongoose, { usernameField: 'email' });
+
+module.exports = mongoose.model('User', User);
